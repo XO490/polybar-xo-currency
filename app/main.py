a7e7693ccf6450f5c230052f885b10377fd988a9
XO490 <xo490@protonmail.com>
2020-05-11 13:39:23 +0700

    init commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+#!env/bin/python
+# coding: utf-8
+# Polibar XO Currency
+# https://t.me/XO490
+
+import requests
+import json
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
